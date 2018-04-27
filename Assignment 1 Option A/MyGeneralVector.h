02282b9400d2ed3e1672f790067df0e2f4c1af6d
[Chelsea Jang] <38049806+ChelseaJang@users.noreply.github.com>
2018-04-27 11:30:39 -0700

    Created private variables for templated vector class

--- Assignment 1 Option A/MyGeneralVector.h
+++ Assignment 1 Option A/MyGeneralVector.h
@@ -1,4 +1,20 @@
 #ifndef ___MY_VECTOR___
 #define ___MY_VECTOR___
 
+namespace pic10C {
+	template<typename objectType>
+	class vector {
+	private:
+		objectType *the_data; // should I use a smartpointer?
+		size_t size; // size of vector
+		size_t capacity; // the capacity of vector
+
+	public:
+		vector() :the_data(nullptr), size(0), capacity(20) {
+			the_data = new double[capacity];
+		}
+
+	};
+}
+
 #endif
